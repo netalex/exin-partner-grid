951af5cec8cd9672e7f4fbbb60d727c4ad778706
Alessandro Aprile <aprile.alessandro@gmail.com>
2025-02-28 10:05:25 +0100

    AA: setup initial application structure and add grid container component
    - Initialize project with basic configuration
    - Set up development environment
    - Add essential dependencies
    - Create GridContainer component
    - Implement basic layout and styling
    - Integrate GridContainer into the main application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainGridComponent } from "../features/main-grid/main-grid.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, MainGridComponent],
+  template: `
+  <div class="center-container">
+
+  <app-main-grid />
+  </div>
+  ` ,
+  styles: [`
+  .center-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Altezza totale della viewport */
+}
+
+  `]
 })
 export class AppComponent {
   title = 'exin-partner-grid';
---
+++ src/features/main-grid/main-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-grid',
+  imports: [],
+  template: `
+    <p>
+      main-grid works!
+    </p>
+  `,
+  styles: ``
+})
+export class MainGridComponent {
+
+}
